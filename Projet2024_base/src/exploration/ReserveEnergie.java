e95b0226dd26e64f797b4ecbd592d881cf4cc21c
JulietteBuriller <166819347+JulietteBuriller@users.noreply.github.com>
2024-04-14 17:01:03 +0200

    Update ReserveEnergie.java

--- Projet2024_base/src/exploration/ReserveEnergie.java
+++ Projet2024_base/src/exploration/ReserveEnergie.java
@@ -13,8 +13,22 @@ public class ReserveEnergie extends Objet
     private final static int MAX = 9;
     private int uenergie;
 
+    public ReserveEnergie(int disponible){
+        super("<symbole>","nom");
+        setUenergie(disponible);
+    }
+    
+    public ReserveEnergie()
+    {
+        this((int)(1+ReserveEnergie.MAX*Math.random()));
+    }     
+    
     public int getuReserveEnergie(){
         return this.uenergie;
+    }
+
+    private void setUenergie(int uenergie) {
+        this.uenergie = uenergie;
     }
     
     @Override
@@ -24,13 +38,14 @@ public class ReserveEnergie extends Objet
         if (this.uenergie!=0){
             int e=Lire.i("Combien en voulez-vous : ");
             if (e<=this.uenergie){
-                jenergie=jenergie+e;
+                jenergie= jenergie + e;
                 this.uenergie=this.uenergie-e;
+                j.setUEnergie(jenergie);
             }
-            else
-            {
+            else if(e>this.uenergie){
                 jenergie=jenergie+this.uenergie;
                 this.uenergie=0;
+                j.setUEnergie(jenergie);
                 System.out.println("Il restait "+this.uenergie+" unités d'énergie dans cette réserve.\nVous ne pouviez donc pas en prendre "+e+"On vous a donc rajouté tout ce qu'il restait dans la réserve");
         }
         else System.out.println("il ne reste plus d'unités d'énergie dans cette réserve.");
@@ -40,14 +55,5 @@ public class ReserveEnergie extends Objet
         */
     }
 
-    public ReserveEnergie(int disponible)
-    {
-        super("<symbole>","nom");
-        //suite code
-    }
-    
-    public ReserveEnergie()
-    {
-        this((int)(1+ReserveEnergie.MAX*Math.random()));
-    }      
-}
+  }
+}   
