c2a6ebbd952196752290fc6f576f3333d3284812
Louise Touvrey <166919190+louisetouvrey@users.noreply.github.com>
2024-04-15 08:48:40 +0200

    Update CaisseGrenades.java

--- Projet2024_base/src/exploration/CaisseGrenades.java
+++ Projet2024_base/src/exploration/CaisseGrenades.java
@@ -13,7 +13,7 @@ public class CaisseGrenades extends Objet
     private final static int MAX = 9;// nombre maximal dans les caisses de grenades trouvées dans les salles
     private int nbgren;
 
-     public CaisseGrenades(int nbGrenades)
+    public CaisseGrenades(int nbGrenades)
     {
         super("<symbole>","nom");
         setNbgren(nbGrenades);
@@ -37,30 +37,28 @@ public class CaisseGrenades extends Objet
     }
 
     @Override
-   public void interaction(Joueur j)
+    public void interaction(Joueur j)
     {
-        int grenade=j.getNbgrenades();
         if (this.nbgren!=0){
             int g=Lire.i("Rentrer un nombre de grenades : ");
             if (this.nbgren>=g){
-                grenade=grenade+g;
+                j.setNbgrenades(j.getNbgrenades()+g);
                 this.nbgren=this.nbgren-g;
-                j.setNbgrenades(grenade);
             }
             else {
-                grenade=grenade+this.nbgren;
+                j.setNbgrenades(j.getNbgrenades()+this.nbgren);
+                System.out.println("Seulement "+this.nbgren+" grenades ont été ajoutées car il n'en restait plus assez dans la réserve.");  
                 this.nbgren=0;
-                j.setNbgrenades(grenade);
-                System.out.println("Seulement "+this.nbgren+" grenades ont été ajoutées car il n'en restait plus assez dans la réserve");  
             }
         }
-        else System.out.println("il ne reste plus de grenades dans cette réserve");
+        else System.out.println("Il ne reste plus de grenades dans cette réserve.");
         
         /* S'il reste des grenades dans cette réserve,
            propose au joueur d'en prendre un certain nombre,
            ce qui augmente les grenades du joueur et diminue d'autant cette réserve
         */
     }
+      
 
    
 }
