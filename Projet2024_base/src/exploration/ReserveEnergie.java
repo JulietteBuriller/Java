3f248aa67ddd34432f12ec673ff6fafa477f3d82
Louise Touvrey <166919190+louisetouvrey@users.noreply.github.com>
2024-04-17 10:24:16 +0200

    Update ReserveEnergie.java

--- Projet2024_base/src/exploration/ReserveEnergie.java
+++ Projet2024_base/src/exploration/ReserveEnergie.java
@@ -26,32 +26,28 @@ public class ReserveEnergie extends Objet
     public int getUenergie(){
         return this.uenergie;
     }
-
     private void setUenergie(int uenergie) {
         this.uenergie = uenergie;
     }
-    @Override
-    public int getNb(){
-        return uenergie;
-    }
+   
     @Override
     public void interaction(Joueur j)
     {
         if (this.uenergie!=0){
-            int e=Lire.i("Combien souhaitez-vous prendre d'unites d'energie ?(Il en reste "+uenergie+".)");
+            int e=Lire.i("Combien souhaitez-vous prendre d'unites d'energie ? (Il en reste "+uenergie+".)");
             
             if (e<=this.uenergie){
                 j.setUEnergie(j.getUEnergie()+e);
                 this.uenergie=this.uenergie-e;
             }
             else {
-                j.setUEnergie(j.getUEnergie()-this.uenergie);
-                System.out.println("Seulement "+this.uenergie+" unités d'energie ont ete ajoutées car il n'en restait plus assez dans la reserve.");  
+                j.setUEnergie(j.getUEnergie()+this.uenergie);
+                System.out.println("Seulement "+this.uenergie+" unites d'energie ont ete ajoutees car il n'en restait plus assez dans la reserve.");  
                 this.uenergie=0;
                 }
         }
         else 
-        System.out.println("il ne reste plus d'unités d'énergie dans cette réserve.");}
+        System.out.println("il ne reste plus d'unites d'energie dans cette reserve.");}
         /* S'il reste des unités d'énergie dans cette réserve,
            propose au joueur d'en prendre un certain nombre,
            ce qui augmente l'énergie du joueur et diminue d'autant cette réserve
