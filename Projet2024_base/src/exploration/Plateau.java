a012f30f96a6e08fc91284b364cc1d0855786b1c
JulietteBuriller <166819347+JulietteBuriller@users.noreply.github.com>
2024-04-19 19:57:06 +0200

    Update Plateau.java

--- Projet2024_base/src/exploration/Plateau.java
+++ Projet2024_base/src/exploration/Plateau.java
@@ -5,6 +5,7 @@
 package exploration;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  *
@@ -28,13 +29,13 @@ public class Plateau
         int nb=(int)(1+4*Math.random());
         switch (nb){
             case 1 : 
-                this.sortie.setPosition(new Position(1,1,this));break;
+                this.sortie= new Salle(new Position(1,1,this),this); this.placeSalle(sortie);break;
             case 2 : 
-                this.sortie.setPosition(new Position (1,this.getNbCol(),this));break;
+                this.sortie= new Salle(new Position (1,this.getNbCol(),this),this);this.placeSalle(sortie);break;
             case 3 : 
-                this.sortie.setPosition (new Position (this.getNbLig(),1,this));break;
+                this.sortie= new Salle(new Position (this.getNbLig(),1,this),this);this.placeSalle(sortie);break;
             case 4 : 
-                this.sortie.setPosition(new Position (this.getNbLig(),this.getNbCol(),this));
+                this.sortie= new Salle(new Position (this.getNbLig(),this.getNbCol(),this),this);this.placeSalle(sortie);break;
                 
         //On prend un nombre au hasard entre 1 et 4, et la sortie est choisie en fonction du nombre obtenu (la sortie est forcément un des 4 coins) 
         //J'ai mis setPosition(Position position) en protected pour que ça marche
@@ -86,17 +87,7 @@ public class Plateau
     public Joueur getJoueur(){return this.joueur;}
     private void setJoueur(Joueur joueur){this.joueur = joueur;}
     public Position getPosJoueur(){return this.getJoueur().getPosition();}
-
-    /*@Override
-    public String toString()
-    {
-        for(int i=1;i<this.getNbLig();i++) // Boucle pour décrire les lignes
-            for(int j=1;j<this.getNbCol();j++) 
-                return this.getSalle(i,j).toString()+" ";// restitue la chaîne qui représente l'ensemble du plateau en combinant les toString des salles (voir exemple dans le sujet)
-            return " _\n";
-    }
-    */
-
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -118,13 +109,43 @@ public class Plateau
         Salle[][] grille1;
         grille1 = new Salle [nbLig][nbCol];
         setGrille(grille1);
-        for(int i=1;i<=nbLig;i++) // Boucle pour décrire les lignes
-            for(int j=1;j<=nbCol;j++){ // Boucle pour décrire les colonnes  
-             int x=(int)(Math.random()*this.jeu.getListeCategories().length);
-             Objet o;
-            o = this.jeu.getListeCategories()[x].getNouveau();
-            this.setNouvelleSalle(new Position (i,j,this),o);
+        
+        int Taille=nbLig*nbCol;
+        Objet[] objets = new Objet[Taille];
+        int nbTotal = jeu.getProportionVides();
+        for(int i = 0 ; i<jeu.getListeCategories().length ; i++) // Parcours des catégories
+        {
+            nbTotal = nbTotal+jeu.getListeCategories()[i].getProportion();
+        }
+        // nbTotal contient désormais la somme des proportions des différents objets à créer
+        /* crée autant d'objets qu'il y a de cases dans le tableau d'objets (ici 100) dans les proportions voulues */
+      
+        int indiceObjet = 0;
+        for(int indiceCategorie = 0 ; indiceCategorie<jeu.getListeCategories().length ; indiceCategorie++)
+        {
+            int nbObjets = ((Taille*jeu.getListeCategories()[indiceCategorie].getProportion())/nbTotal); // calcule le nombre d'objets nbObjets de la catégorie courante à créer
+            System.out.println(nbObjets+" de la catégorie "+jeu.getListeCategories()[indiceCategorie].getClasse().getSimpleName());
+                for(int compteurObjets = 1 ; compteurObjets<= nbObjets ; compteurObjets++,indiceObjet++){ // crée nbObjets instances de la catégorie courante
+                    Objet o = jeu.getListeCategories()[indiceCategorie].getNouveau();
+                    objets[indiceObjet] = o;
             }
+        }
+        Random rand = new Random();
+        for (int i = Taille - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            
+            Objet temp = objets[i];
+            objets[i] = objets[index];
+            objets[index] = temp;
+        }
+        int tmp=0;
+        for(int i=1;i<=nbLig;i++) {
+            for(int j=1;j<=nbCol;j++){
+                this.setNouvelleSalle(new Position (i,j,this),objets[tmp]);
+                tmp++;
+            }
+        }
+        this.setNouvelleSalle(new Position (Taille/2,this));
         this.setJoueur(new Joueur(new Position(((nbLig*nbCol)/2),this)));
         Joueur j = this.getJoueur();
         j.getSalle().setVisible(true);
