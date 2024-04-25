447322e2e521ffe65a5ecdc54a52d1e4dbc5122a
JulietteBuriller <166819347+JulietteBuriller@users.noreply.github.com>
2024-04-25 15:03:30 +0200

    Update UniteDeDeminage.java

--- Projet2024_base/src/exploration/UniteDeDeminage.java
+++ Projet2024_base/src/exploration/UniteDeDeminage.java
@@ -12,20 +12,6 @@ package exploration;
     public class UniteDeDeminage extends Outil{
     
      private Direction directionCourante;
-
-    public void setDirectionCourante (Direction d){
-        this.directionCourante=d;
-    }
-        
-    public Direction getDirectionCourante(){
-        return this.directionCourante;
-    }
-    
-    @Override
-    public void interaction(Joueur j){
-     System.out.println("Cette salle contient une unite de deminage");
-     j.recupere(this);
-    }
     
     public UniteDeDeminage()
     {
@@ -39,26 +25,28 @@ package exploration;
         );
     }    
 
-  
+    protected void setDirectionCourante()
+    {
+        do
+        {
+            this.directionCourante = new Direction(Lire.S("Entrez une direction en combinant 'h','b','g','d' ou 'haut','bas','gauche','droite'"));
+        }while(!directionCourante.isValide());
+    }
     
     @Override
     public void activation(Joueur j)
     {
-        this.setDirectionCourante(this.directionCourante.getDirectionQuelconque());
+        this.setDirectionCourante();
         Salle salle = j.getPosition().getPlateau().getSalle(j.getPosition().getSuivante(this.directionCourante));
 
         if(!j.getSalle().isPossible(this.directionCourante)){
             System.out.println("Il y a un mur !");
         }
-        else
-        {
-            if(new Mine().equals(salle.getContenu())){
+        else{if(new Mine().equals(salle.getContenu())){
                 salle.setObjet(null);
-                System.out.println("la mine a ete desactivee");
-            }
-            else{
-                System.out.println("Il n'y a pas de mine dans cette salle vous avez perdu votre unite de deminage");}
-                j.getOutils().enlève(this);}
+            }else{
+            System.out.println("Il n'y a pas de mine dans cette salle vous avez perdu votre unitee de deminage");}
+           j.getOutils().enlève(this);}
         
         }
         
