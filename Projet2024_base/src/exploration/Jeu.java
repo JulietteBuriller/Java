62e19b9be64b46131a93af294e5517a9c8b5329f
JulietteBuriller <166819347+JulietteBuriller@users.noreply.github.com>
2024-04-28 18:57:09 +0200

    Update Jeu.java

--- Projet2024_base/src/exploration/Jeu.java
+++ Projet2024_base/src/exploration/Jeu.java
@@ -41,13 +41,8 @@ public class Jeu
     public void initJeu(){
         System.out.println("EXPLODING GAME");
         System.out.println("Le but du jeu est de trouver la sortie qui se situe dans l'une des salle du coin du plateau,\nmais gare aux mines si jamais vous entrez dans une salle qui en contient une c'est la mort assuree !");
-        System.out.println("Pour cela vous disposez de materiel de depart(grenades pour vous deplacer/unites d'energie pour utiliser des outils),\ncependant vous pouvez aussi en acquerir durant le jeu, en entrant dans certaines salles.");
-        
-        System.out.println("voici une petit rappel de ce qui existe :");
-        for(int i=0;i<this.getJoueur().getOutils().getTaille();i++){
-        System.out.println(this.getJoueur().getOutils().get(i).getDescriptif()+"("+this.getJoueur().getOutils().get(i).getSymbole()+")");
-        }
-        System.out.println("Voici le plateau, pour l'instant aucune salle n'est visible a part celle ou vous vous trouvez,\nelles apparaitrons quand vous entrerez a l'interieur. \nBonne chance !");
+        System.out.println("Pour cela vous disposez de materiel de depart(outil/grenades pour vous deplacer/unites d'energie pour utiliser des outils),\ncependant vous pouvez aussi en aquerir durant le jeu, en entrant dans certaines salles.");
+        System.out.println("Voici le plateau, pour l'instant aucune salle n'est visible a part celle ou vous vous trouvez,\nelles apparaitrons quand vous entrerz a l'interieur. \nBonne chance !");
     }
     
    private void joue()
@@ -56,49 +51,54 @@ public class Jeu
         while (!isFini())
         {
             System.out.println(" ");
+            System.out.println (this.getJoueur().toString());
             System.out.println (this.getPlateau().toString());
             System.out.println("Que voulez vous faire ?");
             
-            System.out.println("1- Consulter mon materiel");
-            System.out.println("2- Lancer une grenade");
-            System.out.println("3- Aller dans une salle accessible");
-            System.out.println("4- Utiliser un outil");
-            System.out.println("5- Abandonner");
-            int choix = Lire.i("Rentrer 1,2,3,4,ou 5");
-            System.out.println(" ");
+            System.out.println("1- Lancer une grenade\n2- Aller dans une salle accessible\n3- Utiliser un outil\n4- Abandonner\n5- Afficher le tableau (masquer) ");
+            int choix = Lire.i("Rentrer 1,2,3,4 ou 5");
+            System.out.println();
             
             switch (choix){
+
                 case 1:
-                        System.out.println("Vous avez :\n"+this.getJoueur().toString());
-                        break;
-                case 2:
                         System.out.println("Entrez la direction dans laquelle vous souhaitez lancer votre grenade");
                         Direction d1 =Direction.getDirectionHorizontaleVerticale();
                         this.getJoueur().lanceGrenade(d1);
                         
                     break;
-                case 3:
-                    System.out.println("Entrez la direction de la salle dans laquelle vous souhaitez aller");
+                case 2:
+                    System.out.println("Entrez la direction de la salle dans laquelle vous souhaitez aller :");
                     Direction d2 = Direction.getDirectionQuelconque();
                     this.getJoueur().avance(d2);
                     break;
-                case 4: 
-                    System.out.println("Quel outil souhaitez-vous utiliser ? vous possedez "+this.getJoueur().getOutils().toString());
+                case 3: 
+                    System.out.println("Quel outil souhaitez-vous utiliser ? vous possedez ");
+                    for(int i=0;i<this.getJoueur().getOutils().getTaille();i++){
+                    System.out.println(i+1+"- "+this.getJoueur().getOutils().get(i).getNature()+" "+this.getJoueur().getOutils().get(i).getSymbole());}
                     int x=0;
                     do{
-                    x = Lire.i("Rentrer le numero de la place ou se situe l'objet dans la liste (entier) ");
+                    x = Lire.i("Rentrer le numero de la place ou se situe l'objet dans la liste (ou 0 pour +d'info)");
                     x=x-1;
+                    if(x==-1)
+                        for(int i=0;i<this.getJoueur().getOutils().getTaille();i++){
+                        System.out.println(this.getJoueur().getOutils().get(i).getDescriptif()+"("+this.getJoueur().getOutils().get(i).getSymbole()+")");}
                     }
                     while(x<0 || x>=this.getJoueur().getOutils().getTaille());
                     this.getJoueur().getOutils().get(x).utilise(this.getJoueur());
                     break;
-                case 5: 
+                case 4: 
                     this.getJoueur().setPerdant(true);
                     System.out.println("Vous avez perdu...");
                     break;
+                case 5 :
+                    
+                    this.getPlateau().setVisible(this.getPlateau().isVisible()!=true);
             }
             
         }
+        this.getPlateau().setVisible(true);
+        System.out.println (this.getPlateau().toString());
         
         // Déroulement du jeu
     }
