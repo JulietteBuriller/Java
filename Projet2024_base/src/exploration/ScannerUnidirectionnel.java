af5014e3cd0a63b1004a6929c603e9471d277b24
JulietteBuriller <166819347+JulietteBuriller@users.noreply.github.com>
2024-04-28 18:43:33 +0200

    Update ScannerUnidirectionnel.java

--- Projet2024_base/src/exploration/ScannerUnidirectionnel.java
+++ Projet2024_base/src/exploration/ScannerUnidirectionnel.java
@@ -4,45 +4,54 @@
  */
 package exploration;
 
+
 /**
  *
- * @author buril
+ * @author jo
  */
-
-    public class UniteDeDeminage extends Outil{
+public class ScannerUnidirectionnel extends Outil
+{
+    private static final double MARGE_ERREUR = 0.2;
+    private Direction directionCourante;
     
-     private Direction directionCourante;
+    private double getMarge(){return ScannerUnidirectionnel.MARGE_ERREUR;}
+    public Direction getDirectionCourante(){
+        return this.directionCourante;
+    }
     
-    public UniteDeDeminage()
-    {
-        super
-        (
-                "(*)",
-                "Unite de deminage",
-                "- Une unite de deminage, qui desactive moyennant 2 unites d'energie une mine situee dans\n" +
-                "une salle contigue accessible (le mur entre les salles doit deja avoir ete detruit).",
-                8//coût énergétique de la détection des mines
-        );
-    }    
-
     protected void setDirectionCourante(Direction d)
     {
         this.directionCourante=d;
     }
-    
+    public ScannerUnidirectionnel()
+    {
+        super
+        (
+                "}V",
+                "Scanner unidirectionnel",
+                "- Un scanner unidirectionnel detecte a travers les murs la distance (en nombre de salles vides traversee) \n" 
+                +"a laquelle se situe le premier objet (ou le mur du plateau )\n"
+                +"dans une direction donnee. L'estimation de la distance est approximative : elle\n" +
+                "est a 20% de la distance pres. L'utilisation de cet outil necessite 2 unites d'Energie. ",
+                2//coût énergétique de l'utilisation du scanner
+        );
+    }    
+
     @Override
     public void activation(Joueur j)
     {
         this.setDirectionCourante(Direction.getDirectionQuelconque());
-        Salle salle = j.getPosition().getPlateau().getSalle(j.getPosition().getSuivante(this.directionCourante));
+        int salle=0;
+        Position tmp = j.getPosition().getSuivante(directionCourante);
+        while ((tmp.isValide() && tmp.getPlateau().getSalle(tmp).isVide())){
+            tmp=tmp.getSuivante(directionCourante);
+            salle=salle+1;
+        }
+        
+       
+        int marge1=(int)Math.abs(salle-(this.getMarge()*salle));
+        int marge2=(int)Math.abs(salle+(this.getMarge()*salle));
 
-        if(!j.getSalle().isPossible(this.directionCourante)){
-            System.out.println("Il y a un mur !");
-        }
-        else{if(new Mine().equals(salle.getContenu())){
-                salle.setObjet(null);
-            }else{
-            System.out.println("Il n'y a pas de mine dans cette salle vous avez perdu votre unitee de deminage");}
-        }
+        System.out.println("Le nombre de salles vides dans cette direction est compris entre "+marge1+" et "+marge2+".");
     }
-}        
+}
